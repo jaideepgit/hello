422d8317237346613ad7c989cce25e4d79e6ce37
jaideepgit <jaideep20007@gmail.com>
2016-09-30 17:49:02 +0530

    first commit

---
+++ tmp.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+clear
+#sudo rm -R /tmp/*
+#sudo cp /var/log/* /home/server/Desktop/log
+#sudo scp /var/log/* amandeep@192.168.0.109:/home/amandeep/Desktop/jai
+sudo chmod 000 -R amandeep@192.168.0.109:/home/amandeep/Desktop/jai/*
+# hello
